c045f75b47a4bbb623381149e81c7bf572bd26e9
Mathieu Dutour Sikiric <mathieu.dutour@gmail.com>
2024-10-15 14:44:39 +0200

    Some update.

--- setup.py
+++ setup.py
@@ -9,23 +9,27 @@ class BuildCppWithCMake(build_ext):
         repo_url = "https://github.com/MathieuDutSik/polyhedral_common"
         clone_dir = "cpp_code_repo"
 
+        print("BuildCppWithCMake, Step 1")
         if not os.path.exists(clone_dir):
             print(f"Cloning repository from {repo_url}...")
             subprocess.check_call(['git', 'clone', '--recursive', repo_url, clone_dir])
 
+        print("BuildCppWithCMake, Step 2")
         # Step 2: Run CMake to build the C++ project
         build_dir = os.path.join(clone_dir, 'build')
 
         if not os.path.exists(build_dir):
             os.makedirs(build_dir)
 
+        print("BuildCppWithCMake, Step 3")
+
         print("Configuring CMake project...")
         subprocess.check_call(['cmake', '..'], cwd=build_dir)
 
+        print("BuildCppWithCMake, Step 4")
+
         print("Building the C++ code...")
         subprocess.check_call(['cmake', '--build', '.'], cwd=build_dir)
-
-
 
         # Step 3: Copy the generated binaries (artifacts) to the Python package directory
         binaries = ["POLY_SerialDualDesc", "CP_TestCopositivity", "CP_TestCompletePositivity", "LORENTZ_FundDomain_AllcockEdgewalk", "POLY_FaceLatticeGen", "INDEF_FORM_AutomorphismGroup", "INDEF_FORM_TestEquivalence", "INDEF_FORM_GetOrbitRepresentative", "INDEF_FORM_GetOrbit_IsotropicKplane", "LATT_canonicalize", "LATT_FindIsotropic"]
@@ -34,6 +38,8 @@ class BuildCppWithCMake(build_ext):
         if not os.path.exists(target_bin_dir):
             os.makedirs(target_bin_dir)
 
+        print("BuildCppWithCMake, Step 5")
+
         for binary in binaries:
             # Assuming the binaries are located in the 'build' directory
             binary_path = os.path.join(build_dir, binary)
@@ -41,6 +47,8 @@ class BuildCppWithCMake(build_ext):
                 subprocess.check_call(['cp', binary_path, target_bin_dir])
             else:
                 print(f"Warning: {binary} was not found in {build_dir}")
+
+        print("BuildCppWithCMake, Step 6")
 
         # Now, let setuptools do its normal build_ext stuff (optional if you have other extensions)
         super().run()
@@ -55,7 +63,6 @@ setup(
     name='py_polyhedral',
     version='0.1',
     packages=['py_polyhedral'],
-    ext_modules=[cpp_extension],  # Optional: depends on how you're using the C++ code
     cmdclass={
         'build_ext': BuildCppWithCMake,  # Use the custom command to build the C++ code
     },
